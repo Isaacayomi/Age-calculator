ad83e204e86a7657d3122d4f8335b7f2ce0cfc6f
Isaac Ayomide <okunlolaa3@outlook.com>
2024-03-26 23:09:49 +0100

    validating input fields first

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// 'strict mode'
-const inputDay = document.querySelector('.day__field');
-const inputMonth = document.querySelector('.day__field');
-const inputYear = document.querySelector('.day__field');
+"strict mode";
+const inputDay = document.querySelector(".day__field");
+const inputMonth = document.querySelector(".month__field");
+const inputYear = document.querySelector(".year__field");
 
-const submit = document.querySelector('.submit__arrow');
+const submit = document.querySelector(".submit__arrow");
 
-const displayYear = document.querySelector('.year__result')
-const displayMonth = document.querySelector('.month__result')
-const displayDay = document.querySelector('.day__result')
+const displayYear = document.querySelector(".year__result");
+const displayMonth = document.querySelector(".month__result");
+const displayDay = document.querySelector(".day__result");
+
+const currentDate = new Date();
