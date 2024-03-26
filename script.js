1224a7d344a472b1292019fd636029f970b475cc
Isaac Ayomide <okunlolaa3@outlook.com>
2024-03-26 11:29:37 +0100

    logic ip

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-'strict mode'
+// 'strict mode'
+const inputDay = document.querySelector('.day__field');
+const inputMonth = document.querySelector('.day__field');
+const inputYear = document.querySelector('.day__field');
 
+const submit = document.querySelector('.submit__arrow');
+
+const displayYear = document.querySelector('.year__result')
+const displayMonth = document.querySelector('.month__result')
+const displayDay = document.querySelector('.day__result')
